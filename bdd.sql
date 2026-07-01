13ee9927c364537bdffc37c78d6f388bf9a06647
Daniel <daniel537hassan@gmail.com>
2026-07-01 13:02:52 +0200

    ajout bdd

---
+++ bdd.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS "Cours" (
+	"id" INTEGER NOT NULL,
+	"id_matiere" INTEGER NOT NULL,
+	"id_groupe" INTEGER NOT NULL,
+	"semaine_a" BOOLEAN NOT NULL,
+	"semaine_b" BOOLEAN NOT NULL,
+	"date" DATE NOT NULL,
+	"debut" TIME NOT NULL,
+	"fin" TIME NOT NULL,
+	"prof" TEXT NOT NULL,
+	"courriel" TEXT NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY ("id_groupe") REFERENCES "Groupes"("id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+	FOREIGN KEY ("id_matiere") REFERENCES "Matieres"("id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS "Matieres" (
+	"id" INTEGER NOT NULL,
+	"matiere" TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Groupes" (
+	"id" INTEGER NOT NULL,
+	"groupe" TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Newsletter" (
+	"courriel" TEXT NOT NULL,
+	"nom" TEXT NOT NULL,
+	PRIMARY KEY("courriel")
+);
