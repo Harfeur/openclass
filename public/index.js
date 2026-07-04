00a1bc369794f2eda004407ca1823d82730f6e94
Daniel <daniel537hassan@gmail.com>
2026-07-04 22:03:03 +0200

    Ajoute d'evenement au callendrier

---
+++ public/index.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function f() {
+
+        const name = document.getElementById('name').value;
+        if (name.length == 0) {
+            alert("La case Nom ne peut pas etre vide!")
+        } else {
+            fetch('http://localhost:3000/nouveau', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    matiere: 2,
+                    groupe: 4,
+                    semaineA: true,
+                    semaineB: false,
+                    date: "2026-07-03",
+                    timeStart: "08:00:00",
+                    timeEnd: "10:00:00",
+                    nom: name,
+                    email: "daniel537hassan@gmail.com"
+                })
+            }).then((response) => {
+                console.log(response)
+            });
+            console.log(name);
+
+        };
+    }
+    document.getElementById("nouveau").onclick = f;
+});
+
+/*
+
+document.addEventListener("DOMContentLoaded", function () {
+    function f() {
+
+        const name = document.getElementById('name').value;
+        if (name.length == 0) {
+            alert("La case Nom ne peut pas etre vide!")
+        } else {
+            fetch('http://localhost:3000/nouveau', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    matiere: 2,
+                    groupe: 4,
+                    semaineA: true,
+                    semaineB: false,
+                    date: "2026-07-03",
+                    timeStart: "08:00:00",
+                    timeEnd: "10:00:00",
+                    nom: name,
+                    email: "daniel537hassan@gmail.com"
+                })
+            }).then((response) => {
+                console.log(response)
+            });
+            console.log(name);
+
+        };
+    }
+    document.getElementById("nouveau").onclick = f;
+});
+
+
+
+
+
+*/
